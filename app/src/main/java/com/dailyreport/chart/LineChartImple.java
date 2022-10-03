92d0e318bcd67e254e6deb72ecba89bda917c1b9
milhipym <ouyngmin@gmail.com>
2022-10-03 23:23:50 +0900

    //모바일모니터링_@

---
+++ app/src/main/java/com/dailyreport/chart/LineChartImple.java
@@ -0,0 +1,8 @@
+package com.dailyreport.chart;
+
+import com.dailyreport.monitoring.DbMonitorFragment;
+
+public class LineChartImple extends DbMonitorFragment {
+
+    
+}
--- app/src/main/java/com/dailyreport/five/FiveFragment.java
+++ app/src/main/java/com/dailyreport/five/FiveFragment.java
@@ -2,6 +2,7 @@ package com.dailyreport.five;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +14,33 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.dailyreport.MainActivity;
 import com.dailyreport.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FiveFragment extends Fragment {
     public ViewGroup rootview;
+    public FirebaseDatabase database;
+    public DatabaseReference myRef;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview = (ViewGroup) inflater.inflate(R.layout.fragment_shap_five_intro, container, false);
         if (savedInstanceState != null) { }
         initSetting();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("permission_phone");
         return rootview;
     }
 
     private void initSetting() {
+
+        //((DatabaseReference) myRef).setValue("Hello, World!");
+
+
         MainActivity mainActivity = (MainActivity) getActivity();
         //mainActivity.changeFragment("DbMonitorFragment");
         Runnable runnable = () -> mainActivity.changeFragment("DbMonitorFragment");
@@ -33,4 +48,19 @@ public class FiveFragment extends Fragment {
         mHandler.postDelayed(runnable, 2500);
     }
 
+    public DatabaseReference getMyRef() {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("YYYM", "Value is: " + value.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("YYYM", "Failed to read value.", databaseError.toException());
+            }
+        });
+        return myRef;
+    }
 }
